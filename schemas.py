5e25623cca0d6f50beb767ee59a70d505c2f5663
jtweedle1 <jazminrtweedle@gmail.com>
2025-07-28 16:53:28 -0500

    Create schemas for user creation/stream output

---
+++ schemas.py
@@ -0,0 +1,19 @@
+# This file defines Pydantic models that can 1. validate incoming data 2. control what data goes out in responses
+
+# Imports
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+
+# For when a user registers for the first tinme
+class UserIn(BaseModel):
+    email: EmailStr # EmailStr vs. str for better validation
+    name: str
+    password: str
+
+# What's returned after creating or fetching a user; does NOT include password
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    name: str
+    coins_earned: int
+    creation_date: datetime
