d52d8bf0370d321e55f893a9e69f6d9e2b6cd02b
jtweedle1 <jazminrtweedle@gmail.com>
2025-07-28 16:24:18 -0500

    Create connection to future database

---
+++ database.py
@@ -0,0 +1,11 @@
+# Imports
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+ # Database Connection
+engine = create_engine('sqlite:///orm.db')
+
+Session = sessionmaker(bind=engine) # factory used to interact with db; uses engine as bridge to db
+session = Session() # start a new session
+
+Base = declarative_base() # class factory / blueprint for database tables
